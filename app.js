3e0419f0020aa5e13ec05ecb5002fbc4d8c6c6c6
Sebastian Budyński <Sebbudynski@gmail.com>
2024-01-29 20:16:06 +0100

    added coments

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const comment = document.querySelector(".comment");
 const rizky = document.getElementById("rizky");
 const unreadedCounter = document.getElementById("counter");
 
+// Adding event listener to each post and removing red dot
 for (let i = 0; i < posts.length; i++) {
   posts[i].addEventListener("click", function () {
     addReadedState(this);
-      redDots[i].style.display = "none";
-    
+    redDots[i].style.display = "none";
   });
 }
 
+//display comment
 rizky.addEventListener("click", function () {
   if (comment.style.display === 'block'){
     comment.style.display = 'none'
@@ -23,16 +24,19 @@ rizky.addEventListener("click", function () {
   comment.style.display = "block";
 });
 
+// Adding readed state to post and updating unreaded counter
 function addReadedState(post) {
   post.classList.add("readed");
   updatedUnreadedCounter();
 }
 
+// Updating unreaded counter
 function updatedUnreadedCounter() {
   const unreadedPosts = document.querySelectorAll(".post:not(.readed)");
   unreadedCounter.innerText = unreadedPosts.length;
 }
 
+// Mark all as readed
 markAll.addEventListener("click", function markAllAsReaded() {
   for (let i = 0; i < posts.length; i++) {
     posts[i].classList.add("readed");
