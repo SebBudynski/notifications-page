bd051ed32d2dc4e69803d33f2298f243620dd17b
Sebastian Budyński <Sebbudynski@gmail.com>
2024-01-29 11:02:48 +0100

    Fixing problem with deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("style/index.scss").pipe(sass()).pipe(dest("css"));
+  return src("index.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["style/index.scss"], buildStyles);
+  watch(["index.scss"], buildStyles);
 }
 
 exports.build = series(buildStyles);
